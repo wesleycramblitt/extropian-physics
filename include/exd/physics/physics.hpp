653de3adc4728669d4dac388900dd131ca5b5b73
Wesley Cramblitt <wcramblitt@gmail.com>
2026-06-29 14:49:34 -0600

    Fix physics.hpp - remove includes for non-existent headers

--- include/exd/physics/physics.hpp
+++ include/exd/physics/physics.hpp
@@ -2,8 +2,4 @@
 
 // Umbrella header for extropian-physics.
 #include <exd/physics/solver/plugin_interface.hpp>
-#include <exd/physics/solver/solver_manager.hpp>
-#include <exd/physics/solver/time_stepping.hpp>
-#include <exd/physics/solver/convergence.hpp>
-#include <exd/physics/coupling/coupling_manager.hpp>
-#include <exd/physics/coupling/surface_mapping.hpp>
+// Additional headers will be added as public types are promoted from src/ to include/
